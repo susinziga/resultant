13eb35c395cf1a640b81899a6c77c9feaa70b1e5
ziga <ziga.susin@gmail.com>
2022-03-22 17:20:07 +0100

    homepage animacija

---
+++ components/menu_top/Menu_top.styled.js
@@ -0,0 +1,33 @@
+import styledComponents from "styled-components";
+
+export const MenuContainer = styledComponents.div``;
+
+export const LogoContainer = styledComponents.div`
+    position:relative;
+    width:fit-content;
+
+    >object{ 
+        position:absolute;
+  
+        top:-21%;
+        bottom:0;
+        right:0;
+
+        height: 100%;
+  
+        left: 57%;
+        width: fit-content;
+        transform: scale(0.5);
+
+        transform:scale(5);
+
+
+
+        /*  ZAČETNE  */
+
+
+    /*scale 22
+    left: 100%;
+    top: 500%;*/
+    }
+`;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import Menu_top from "../components/menu_top/Menu_top";
 import "../styles/globals.css";
 import "../styles/root.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Menu_top></Menu_top>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
