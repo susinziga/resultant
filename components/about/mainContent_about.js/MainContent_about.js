4502affab37c7e2fd9bc9820e3032653f112767f
chewbaccapush <gasper.lukacs@gmail.com>
2022-03-25 14:56:52 +0100

    .

--- components/about/mainContent_about.js/MainContent_about.js
+++ components/about/mainContent_about.js/MainContent_about.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { MainContentContainer, MainContentHeading, MainContentLine, MainContentNavBar } from './MainContent.styled';
 import useTranslation from "next-translate/useTranslation";
+
+import gsap from "gsap";
 
 
 const MainContent_about = (props) => {
@@ -8,6 +10,7 @@ const MainContent_about = (props) => {
     const [selected2, setSelected2] = useState(false);
     const [selected3, setSelected3] = useState(false);
     const [selected4, setSelected4] = useState(false);
+    const lineAnimation = useRef(null);
 
   const { t, lang } = useTranslation();
 
@@ -16,20 +19,34 @@ const MainContent_about = (props) => {
   const navItem3 = t("about:nav_item3");
   const navItem4 = t("about:nav_item4");
 
+  const [centered, setCentered] = useState(false);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e, contentRef) => {
+      handleScroll(e, contentRef);
+    });
+  }, []);
+
+
   const handleScroll = (e) => {
-    console.log("gas2");
-    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-    if (bottom) { 
-        console.log("gas");
-     }
+    let contentPosition = contentRef.current.getBoundingClientRect().y;
+    if(contentPosition === 0){
+        gsap.to(lineAnimation, {
+            duration: 0.5,
+            width: 0,
+          });
+    };
+    
+     
   }
  
 
   return (<>
-    <MainContentContainer {...props} onScroll={handleScroll}>
+    <MainContentContainer {...props} ref={contentRef} onScroll={handleScroll}>
         <MainContentNavBar>
             <MainContentHeading selected={selected1}>{navItem1}</MainContentHeading>
-            <MainContentLine ></MainContentLine>
+            <MainContentLine  ref={(el) => (lineAnimation = el)}></MainContentLine>
             <MainContentHeading selected={selected2}>{navItem2}</MainContentHeading>
             <MainContentHeading selected={selected3}>{navItem3}</MainContentHeading>
             <MainContentHeading selected={selected4}>{navItem4}</MainContentHeading>
