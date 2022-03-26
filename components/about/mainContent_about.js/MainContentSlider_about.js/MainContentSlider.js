c0df43ba0f6552033c447112c77e2827c67c735d
ziga <ziga.susin@gmail.com>
2022-03-26 14:32:13 +0100

    onas swiper

--- components/about/mainContent_about.js/MainContentSlider_about.js/MainContentSlider.js
+++ components/about/mainContent_about.js/MainContentSlider_about.js/MainContentSlider.js
@@ -1,34 +1,66 @@
 import React from "react";
 
-import {Container } from "./MainContentSlider.styled";
+import { Container } from "./MainContentSlider.styled";
 
 import { useSwiper } from "swiper/react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, {
-    Mousewheel,Pagination
-  } from 'swiper';
+import SwiperCore, { Mousewheel, Pagination } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
 import References_item from "../../../references/references_item/References_item";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import Content_item from "../contentSliderItem.js/ContentSliderItem";
 
-SwiperCore.use([Mousewheel,Pagination]);
+SwiperCore.use([Mousewheel, Pagination]);
 
-const MainContentSlider = () => {
-
+const MainContentSlider = ({ nextSection, prevSection }) => {
   const items = [
-    {name: "1. Poglobljena analiza stanja", desc:"Projekte začnemo s pridobivanjem poglobljenega razumevanja poslovne situacije in zahtev naših naročnikov. Podrobno analiziramo obstoječe stanje, pri čemer uporabljamo preverjena orodja in metodologije."},
-    {name: "2. Opredelitev projektnih standardov", desc:"Opredelimo standarde, kriterije in merila, ki jih kasneje vpeljemo v projekt. Izhodišče nam predstavlja strategija naročnika. Ne osredotočamo se le na kratkoročne rezultate, ampak na izgradnjo celostne rešitve, ki zagotavlja trajno uspešnost."},
-    {name: "3. Implementacija nove rešitve", desc:"Skupaj z osebjem naročnika izvedemo implementacija rešitev in želenih izboljšav v poslovni proces naročnika. Naš pristop zagotavlja individualno obravnavo posebnosti in izzivov vsakega naročnika, na osnovi katerih pripravimo inovativne pristope in po meri naročnika ustvarjene rešitve"},
-    {name: "4. Usposabljanje vodij ", desc:"Nove rešitve bodo v praksi delovale samo, če jih bodo uporabniki znali pravilno in celovito uporabljati. Zato v tem koraku usposobimo vodje in strokovno osebje za trajno uporabo implementiranih rešitev. Ob tem še posebej poskrbimo, da se zagotovi trajen prenos znanja za upravljanje rešitev na interno osebje naročnika."},
-    {name: "5. Vzdrževanje in podpora ", desc:"Po zaključenem procesu izvedemo monitoring delovanja novega procesa. V večini primerov ponovno izvedemo merjenje obstoječega stanja notranjega okolja in spremljamo trende v organizacijski klimi. Po potrebi izvedemo nadgradnjo vpeljane nove rešitve. Ob tem stalno in sistemsko podpiramo interne skrbnike novo implementiranih rešitev do zaključene vpeljave v prakso."}
+    {
+      name: "1. Poglobljena analiza stanja",
+      desc: "Projekte začnemo s pridobivanjem poglobljenega razumevanja poslovne situacije in zahtev naših naročnikov. Podrobno analiziramo obstoječe stanje, pri čemer uporabljamo preverjena orodja in metodologije.",
+    },
+    {
+      name: "2. Opredelitev projektnih standardov",
+      desc: "Opredelimo standarde, kriterije in merila, ki jih kasneje vpeljemo v projekt. Izhodišče nam predstavlja strategija naročnika. Ne osredotočamo se le na kratkoročne rezultate, ampak na izgradnjo celostne rešitve, ki zagotavlja trajno uspešnost.",
+    },
+    {
+      name: "3. Implementacija nove rešitve",
+      desc: "Skupaj z osebjem naročnika izvedemo implementacija rešitev in želenih izboljšav v poslovni proces naročnika. Naš pristop zagotavlja individualno obravnavo posebnosti in izzivov vsakega naročnika, na osnovi katerih pripravimo inovativne pristope in po meri naročnika ustvarjene rešitve",
+    },
+    {
+      name: "4. Usposabljanje vodij ",
+      desc: "Nove rešitve bodo v praksi delovale samo, če jih bodo uporabniki znali pravilno in celovito uporabljati. Zato v tem koraku usposobimo vodje in strokovno osebje za trajno uporabo implementiranih rešitev. Ob tem še posebej poskrbimo, da se zagotovi trajen prenos znanja za upravljanje rešitev na interno osebje naročnika.",
+    },
+    {
+      name: "5. Vzdrževanje in podpora ",
+      desc: "Po zaključenem procesu izvedemo monitoring delovanja novega procesa. V večini primerov ponovno izvedemo merjenje obstoječega stanja notranjega okolja in spremljamo trende v organizacijski klimi. Po potrebi izvedemo nadgradnjo vpeljane nove rešitve. Ob tem stalno in sistemsko podpiramo interne skrbnike novo implementiranih rešitev do zaključene vpeljave v prakso.",
+    },
   ];
 
   const swiperInstance = useSwiper();
   const [swip, setSwip] = useState();
+
+  useEffect(() => {
+    window.addEventListener("wheel", handleWheel);
+
+    return () => {
+      return window.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
+  const handleWheel = (e) => {
+    if (e.deltaY > 0) {
+      console.log(swip.isEnd);
+      if (swip.isEnd) {
+        console.log("here");
+        nextSection();
+      } else swip.slideNext();
+    } else {
+      swip.slidePrev();
+    }
+  };
 
   const slide = (nr) => {
     for (let index = 0; index < nr; index++) {
@@ -49,13 +81,12 @@ const MainContentSlider = () => {
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => (swip = swiper)}
-        mousewheel={true}
         direction={"horizontal"}
       >
         {items.map((ref, id) => (
           <SwiperSlide key={id}>
-               {({ isActive }) => (
-            <Content_item reference={ref} isActive={isActive}></Content_item>
+            {({ isActive }) => (
+              <Content_item reference={ref} isActive={isActive}></Content_item>
             )}
           </SwiperSlide>
         ))}
--- components/about/mainContent_about.js/MainContent_about.js
+++ components/about/mainContent_about.js/MainContent_about.js
@@ -1,26 +1,31 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { MainContentContainer, MainContentContent, MainContentHeading, MainContentLine, MainContentNavBar } from './MainContent.styled';
+import React, { useState, useEffect, useRef } from "react";
+import {
+  MainContentContainer,
+  MainContentContent,
+  MainContentHeading,
+  MainContentLine,
+  MainContentNavBar,
+} from "./MainContent.styled";
 import useTranslation from "next-translate/useTranslation";
 import { enableBodyScroll, disableBodyScroll } from "body-scroll-lock";
 
 import gsap from "gsap";
-import MainContentSlider from './MainContentSlider_about.js/MainContentSlider';
-
+import MainContentSlider from "./MainContentSlider_about.js/MainContentSlider";
 
 const MainContent_about = (props) => {
-    const [selected1, setSelected1] = useState(true);
-    const [selected2, setSelected2] = useState(false);
-    const [selected3, setSelected3] = useState(false);
-    const [selected4, setSelected4] = useState(false);
-    const [transition, setTransition] = useState(false);
-    const [initial, setInitial] = useState(true);
-    const [selectedMenu, setSelectedMenu] = useState(1);
-    const [menu, setMenu] = useState(1)
-    const [location, setLocation] = useState("")
-    const lineAnimation = useRef(null);
-    const navAnimation = useRef(null);
-    const contentAnimation = useRef(null);
-    const sliderAnimation = useRef(null);
+  const [selected1, setSelected1] = useState(true);
+  const [selected2, setSelected2] = useState(false);
+  const [selected3, setSelected3] = useState(false);
+  const [selected4, setSelected4] = useState(false);
+  const [transition, setTransition] = useState(false);
+  const [initial, setInitial] = useState(true);
+  const [selectedMenu, setSelectedMenu] = useState(1);
+  const [menu, setMenu] = useState(1);
+  const [location, setLocation] = useState("");
+  const lineAnimation = useRef(null);
+  const navAnimation = useRef(null);
+  const contentAnimation = useRef(null);
+  const sliderAnimation = useRef(null);
 
   const { t, lang } = useTranslation();
 
@@ -38,124 +43,101 @@ const MainContent_about = (props) => {
     });
   }, []);
 
-  useEffect(() => {
-    if (centered) {
-      window.addEventListener("wheel", handleWheel);
-    }
-  }, [centered]);
-
   let selected = 1;
   let counter = 1;
 
+  const nextSection = () => {
+    selected++;
+    setSelectedMenu(selected);
+  };
 
-  const handleWheel = (e) => {
-    
-    if (e.deltaY > 0) {
-      selected++;
-      counter++;
-      setSelectedMenu(selected);
-    } else {
-      selected--;
-      counter--;
-      setSelectedMenu(selected);
-    }
+  const prevSection = () => {
+    selected--;
+    setSelectedMenu(selected);
+  };
 
-    if(counter < 6 ){
-      selected = 1;
-    } 
-    console.log(selectedMenu)
-    console.log(selected);
-    console.log(counter);
-
-    if(selected == 1) {
-      setSelected1(true);
-      setSelected2(false);
-      setSelected3(false);
-      setSelected4(false);
-      
-    } else if (selected == 2) {
-      setSelected1(false);
-      setSelected2(true);
-      setSelected3(false);
-      setSelected4(false);
-  
-    } else if (selected == 3) {
-      setSelected1(false);
-      setSelected2(false);
-      setSelected3(true);
-      setSelected4(false);
-
-    } else if (selected == 4) {
-      setSelected1(false);
-      setSelected2(false);
-      setSelected3(false);
-      setSelected4(true);
-  } else if(selected > 4) {
-    selected = 4; 
-  }
-
-  if(counter <= 0){
-    enableBodyScroll(document.getElementsByTagName("body")[0]);
-    
-  }
-}
-
-
-  const handleEnd = () => {}
+  const handleEnd = () => {};
 
   const handleScroll = (e) => {
     let contentPosition = contentRef.current.getBoundingClientRect().y;
     console.log(contentPosition);
-    if(contentPosition < 1){
-        counter=0;
-        gsap.to(lineAnimation, {
-            duration: 2.5,
-            width: 0,
-        });
-        gsap.to(navAnimation, {   
-            width: "30%",
-            paddingLeft: "0.1%",
-            duration: 2,
-            
-            fontSize: "3rem",
-        });
-        gsap.to(contentAnimation, {
-            paddingTop: "15%",
-            width: "70%",
-            duration: 2,
-           
-        });
-     
-        setInitial(false);
-        setTransition(true);
-        disableBodyScroll(document.getElementsByTagName("body")[0]);
-        setCentered(true);
-    };
-  }
+    if (contentPosition < 1) {
+      counter = 0;
+      gsap.to(lineAnimation, {
+        duration: 2.5,
+        width: 0,
+      });
+      gsap.to(navAnimation, {
+        width: "30%",
+        paddingLeft: "0.1%",
+        duration: 2,
 
+        fontSize: "3rem",
+      });
+      gsap.to(contentAnimation, {
+        paddingTop: "15%",
+        width: "70%",
+        duration: 2,
+      });
 
-  return (<>
-    <MainContentContainer {...props} ref={contentRef} transition={transition}  onScroll={handleScroll}>
+      setInitial(false);
+      setTransition(true);
+      disableBodyScroll(document.getElementsByTagName("body")[0]);
+      setCentered(true);
+    }
+  };
+
+  return (
+    <>
+      <MainContentContainer
+        {...props}
+        ref={contentRef}
+        transition={transition}
+        onScroll={handleScroll}
+      >
         <MainContentNavBar ref={(el) => (navAnimation = el)}>
-            <MainContentHeading initial={initial} selected={selected1} transition={transition}>{navItem1}</MainContentHeading>
-            <MainContentLine  ref={(el) => (lineAnimation = el)}></MainContentLine>
-            <MainContentHeading selected={selected2} transition={transition}>{navItem2}</MainContentHeading>
-            <MainContentHeading selected={selected3} transition={transition}>{navItem3}</MainContentHeading>
-            <MainContentHeading selected={selected4} transition={transition}>{navItem4}</MainContentHeading>
+          <MainContentHeading
+            initial={initial}
+            selected={selectedMenu === 1}
+            transition={transition}
+          >
+            {navItem1}
+          </MainContentHeading>
+          <MainContentLine ref={(el) => (lineAnimation = el)}></MainContentLine>
+          <MainContentHeading
+            selected={selectedMenu === 2}
+            transition={transition}
+          >
+            {navItem2}
+          </MainContentHeading>
+          <MainContentHeading
+            selected={selectedMenu === 3}
+            transition={transition}
+          >
+            {navItem3}
+          </MainContentHeading>
+          <MainContentHeading
+            selected={selectedMenu === 4}
+            transition={transition}
+          >
+            {navItem4}
+          </MainContentHeading>
         </MainContentNavBar>
         <MainContentContent ref={(el) => (contentAnimation = el)}>
-          {
-            selectedMenu == 1 ? <MainContentSlider handleEnd={handleEnd}/> : ""
-            
-          }
-          {
-            selectedMenu == 2 ? "" : ""
-          }
-
+          {selectedMenu == 1 ? (
+            <MainContentSlider
+              nextSection={nextSection}
+              prevSection={prevSection}
+              handleEnd={handleEnd}
+            />
+          ) : (
+            ""
+          )}
+          {selectedMenu == 2 ? "" : ""}
         </MainContentContent>
-    </MainContentContainer>
-  </>
-  )
-}
+      </MainContentContainer>
+    </>
+  );
+};
 
 export default MainContent_about;
