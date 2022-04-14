e3006c5034759f50d44ad3d8c845063da2c045ea
ziga <ziga.susin@gmail.com>
2022-04-15 01:51:36 +0200

    team page, clanek1

--- pages/clanek.js
+++ pages/clanek.js
@@ -2,7 +2,7 @@ import React from "react";
 import Blog_page from "../components/blog/Blog_page";
 
 const clanek = () => {
-  return <Blog_page></Blog_page>;
+  return <></>;
 };
 
 export default clanek;
