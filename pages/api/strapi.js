852f9158bf58d42fe01a270073a43890292ccede
Marko Plaznik <icevx1@gmail.com>
2022-04-28 19:50:02 +0200

    fix err

--- pages/api/strapi.js
+++ pages/api/strapi.js
@@ -19,9 +19,8 @@ export function getArticleFromStrapiData(article) {
 
 export function getStrapiURL(path = "") {
   return `${
-    process.env.NODE_ENV == "production"
-      ? process.env.STRAPI_API_URL
-      : "http://127.0.0.1:1337"
+    // "http://127.0.0.1:1337"
+    "https://resultant-strapi.herokuapp.com"
   }${path}`;
 }
 
