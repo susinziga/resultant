83b71a4a8bd3bb246b14b8a18f8cfeba3e4c0ddc
Marko Plaznik <icevx1@gmail.com>
2022-05-11 18:43:50 +0200

    seo

--- components/SRK/Contact/Form/ContactForm.styled.js
+++ components/SRK/Contact/Form/ContactForm.styled.js
@@ -67,7 +67,7 @@ export const TextareaContainer = styled.div`
   }
 `;
 
-export const ContactHeader = styled(Title2)`
+export const ContactHeader = styled.h3`
   font-family: "Neusa";
   font-weight: 500;
   // margin:10% 0;
