bd99220c8ebf4a5848e1685f36ce1a614bacad99
Marko Plaznik <icevx1@gmail.com>
2022-05-16 12:38:01 +0200

    .

--- pages/clanek/[clanek].js
+++ pages/clanek/[clanek].js
@@ -198,6 +198,7 @@ const Clanek = ({ clanek }) => {
           content={article.glavnaSlika.data.attributes.url}
         />
         <meta
+          key="twitter-image"
           name="twitter:image"
           property="og:image"
           content={article.glavnaSlika.data.attributes.url}
