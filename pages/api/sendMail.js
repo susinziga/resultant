13fc4f85e74c19e6f0e01afc3ffddefe308b6c07
Marko Plaznik <icevx1@gmail.com>
2023-06-30 12:29:44 +0200

    sendgrid

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -2,27 +2,20 @@
 
 export default async function handler(req, res) {
   const { data } = req.body;
-  var nodemailer = require("nodemailer");
-  var smtpTransport = require("nodemailer-smtp-transport");
 
-  var transporter = nodemailer.createTransport(
-    smtpTransport({
-      service: "gmail",
-      // host: "smtp.gmail.com",
-      // secureConnection: true,
-      // secure: true,
-      secure: false,
-      auth: {
-        user: "resultanthsc@gmail.com",
-        pass: "pioyrizwplkrmsfu",
-      },
-    })
-  );
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   let message = "";
   for (let id in data) {
     message += "<b>" + id + "</b>" + " : " + data[id] + "<br/>";
   }
+
+  const msg = {
+    from: "resultanthsc@gmail.com",
+    subject: "RESULTANT POVPRAŠEVANJE",
+    html: message,
+  };
 
   const receivers = [
     // "klaric.enej@gmail.com",
@@ -32,26 +25,18 @@ export default async function handler(req, res) {
     "thecrazy.marko@gmail.com",
   ];
 
-  for (let i = 0; i < receivers.length; i++) {
-    const mailOptions = {
-      from: "RESULTANT POVPRAŠEVANJE <resultanthsc@gmail.com>",
-      name: "RESULTANT",
-      to: receivers[i],
-      subject: data.subject,
-      html: message,
-    };
-
-    console.log("SENDING EMAIL TO " + receivers[i]);
-    await transporter.sendMail(mailOptions, function (err, info) {
-      if (err) {
-        console.error("ERROR SENDING MAIL TO " + receivers[i]);
-        console.error(err);
-      } else {
-        console.log("SENT EMAIL TO " + receivers[i]);
-        console.log(info);
+  sgMail
+    .send({
+      ...msg,
+      to: receivers,
+    })
+    .then(
+      () => console.log("SENT EMAILS"),
+      (error) => {
+        console.error("ERROR SENDING EMAILS");
+        console.error(error);
       }
-    });
-  }
+    );
 
   res.status(200).json({ name: "Sent" });
 }
